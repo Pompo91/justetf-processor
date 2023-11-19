9f17516c35244ed82cdd35c967eb9f5d843aafb8
Jindra <sindelar.jindrich@gmail.com>
2023-11-19 14:54:39 +0100

    load multiple .jsons from a given directory

--- process-jsons.py
+++ process-jsons.py
@@ -1,12 +1,15 @@
 import json
 import argparse
 import datetime
+import os
 
 class series:
+    _name: str
     _data: dict
 
     def __init__(self, json_path: str):
         try:
+            self._name = json_path.split("/")[-1].split(".")[0]
             with open(json_path, "r") as f:
                 whole_json = json.load(f)
                 self._data = whole_json["series"]
@@ -26,6 +29,9 @@ class series:
     def __get_format_string(self) -> str:
         return "%Y-%m-%d"
 
+    def get_name(self) -> str:
+        return self._name
+
     def get_first_datetime(self) -> datetime.datetime:
         return datetime.datetime.strptime(self._data[0]["date"], self.__get_format_string())
 
@@ -44,22 +50,22 @@ class series:
             for point in self._data:
                 f.write("{},{},\n".format(point["date"], point["value"]["raw"]))
         
-    
-
-
 if __name__ == "__main__":
-    arg_parser = arg_parser = argparse.ArgumentParser(description = "Extract CSV from JustETF JSON response")
-    arg_parser.add_argument("-i", "--input-json", type = str, required = True, help = "Path to the input JSON file")
-    arg_parser.add_argument("-o", "--output-csv", type = str, required = True, help = "Path to the output CSV file")
+    arg_parser = arg_parser = argparse.ArgumentParser(description = "Process JSONSs downloaded from JustETF")
+    arg_parser.add_argument("-i", "--input-json-dir", type = str, required = True, help = "Path to directory containing the JSON files")
+    # arg_parser.add_argument("-o", "--output-csv", type = str, required = False, help = "Path to the output CSV file")
 
     args = arg_parser.parse_args()
 
-    
-    whole_json = dict()
-
     try:
-        data_series = series(args.input_json)
-        data_series.write_csv(args.output_csv)
+        if not os.path.isdir(args.input_json_dir):
+            raise Exception("{} is not a directory!".format(args.input_json_dir))
+        
+        series_list = list()
+        for file in os.listdir(args.input_json_dir):
+            if file.endswith(".json"):
+                series_list.append(series(os.path.join(args.input_json_dir, file)))
+                print("Series \"{}\" added".format(series_list[-1].get_name()))
 
     except Exception as exc:
         print("EXCEPTION OCCURRED: {}".format(exc))
